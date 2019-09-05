24a3a6a98f4006b377a8edadb549e54a95bcba4a
davidtazy <david.allemant@gmail.com>
2019-09-06 00:18:55 +0200

    remove grunt

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,33 @@
-//import  Game  from './Game'
+//import * as p5 from '../lib/p5.min.js';
 
+import Game from './Game';
+import Renderer from './Renderer';
+import Inputs from './Inputs';
 
 let game;
+let renderer;
+let inputs;
+let s = (p5js) => {
+    p5js.setup = () => {
+        console.log('setup');
 
-function setup() {
-  createCanvas(640, 480);
+        inputs = new Inputs(p5js);
+        // inputs.register_key_events((key)=>{
+        //     console.log('key event',key.isLeft(),key.isRight());
+        // });
 
-  game = new Game(6, 6, 1);
-}
+        game = new Game(10,10,1);
+        game.setInputs(inputs);
+        renderer = new Renderer(p5js,game);
+    };
 
-function draw() {
-    console.log("draw")
-  background(51);
-}
+    p5js.draw = () => {
+        p5js.background(51);
+
+
+        renderer.draw();
+        
+    };
+};
+
+const myP5 = new p5(s,document.getElementById('p5sketch'));
